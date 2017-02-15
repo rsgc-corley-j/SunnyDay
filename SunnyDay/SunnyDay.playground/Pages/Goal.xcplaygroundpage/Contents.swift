dc49ca0184577c418c359ac2d83c86b9b971ad55
Student <student@macbook-pro-1174-corley.local>
2017-02-15 09:51:36 -0500

    Completed assignment

--- SunnyDay/SunnyDay.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ SunnyDay/SunnyDay.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -23,6 +23,9 @@ canvas.drawShapesWithFill = true
 //canvas.drawEllipse(centreX: canvas.width / 2, centreY: canvas.height / 2 , width: 100, height: 100)
 
 //canvas.LineColor
+canvas.fillColor = Color.blue
+
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 400)
 
 for x in stride(from: 0, through: 500, by: 50){
     
@@ -32,23 +35,53 @@ for x in stride(from: 0, through: 500, by: 50){
         canvas.lineColor = Color.orange
     }
     
-canvas.drawLine(fromX: 0, fromY: 400, toX: x, toY: 0, lineWidth: 3)
+    canvas.drawLine(fromX: 0, fromY: 400, toX: x, toY: 0, lineWidth: 3)
     
-//canvas.drawLine(fromX: 0, fromY: 400, toX: x-50, toY: 0, lineWidth: 3)
+    //canvas.drawLine(fromX: 0, fromY: 400, toX: x-50, toY: 0, lineWidth: 3)
     
-canvas.drawLine(fromX: 0, fromY: 400, toX: 500, toY: x, lineWidth: 3)
+    canvas.drawLine(fromX: 0, fromY: 400, toX: 500, toY: x, lineWidth: 3)
     
-//canvas.drawLine(fromX: 0, fromY: 400, toX: 500, toY: x-50, lineWidth: 3)
+    //canvas.drawLine(fromX: 0, fromY: 400, toX: 500, toY: x-50, lineWidth: 3)
 }
 
 canvas.fillColor = Color.yellow
 canvas.drawEllipse(centreX: 0, centreY: 400 , width: 100, height: 100)
 
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 500, centreY: 350 , width: 100, height: 100)
+
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 450, centreY: 400 , width: 100, height: 100)
+
 canvas.drawShapesWithBorders = true
 canvas.drawShapesWithFill = false
 
+
+
+canvas.borderColor = Color.red
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 475, height: 400, borderWidth: 25)
+
+canvas.borderColor = Color.orange
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 425, height: 350, borderWidth: 25)
+
+canvas.borderColor = Color.yellow
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 375, height: 300, borderWidth: 25)
+
+canvas.borderColor = Color.green
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 325, height: 250, borderWidth: 25)
+
+canvas.borderColor = Color.blue
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 275, height: 200, borderWidth: 25)
+
 canvas.borderColor = Color.purple
-canvas.drawEllipse(centreX: canvas.width / 2, centreY: 0 , width: 100, height: 100)
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 225, height: 150, borderWidth: 25)
+
 
 
 
